97a91d04763a1d315254d9259f7b89d9a9374093
zUnixorn <dev@jonascrull.de>
2024-12-12 01:47:42 +0100

    solve day 11 part 2

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -2,10 +2,7 @@ use advent_of_code::number;
 use nom::bytes::complete::tag;
 use nom::multi::separated_list0;
 use nom::IResult;
-use rayon::iter::plumbing::{Folder, Reducer, UnindexedConsumer};
-use rayon::iter::ParallelIterator;
-use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, IntoParallelRefMutIterator};
-use rayon::prelude::*;
+use std::collections::HashMap;
 
 advent_of_code::solution!(11);
 
@@ -13,123 +10,69 @@ fn parse(input: &str) -> IResult<&str, Vec<u64>> {
     separated_list0(tag(" "), number)(input.trim())
 }
 
-pub fn part_one(input: &str) -> Option<u32> {
-    let (_, mut stones) = parse(input).ok()?;
-
-    for _ in 0..25 {
-        for i in 0..stones.len() {
-            match stones[i] {
-                0 => stones[i] = 1,
-                number => {
-                    let mut digits = 0;
-                    let mut rest = number;
-
-                    while rest > 0 {
-                        rest /= 10;
-                        digits += 1;
-                    }
-
-                    if digits & 1 == 0 {
-                        let half_power = 10u64.pow(digits / 2);
-
-                        stones[i] = number / half_power;
-                        stones.push(number % half_power);
-                    } else {
-                        stones[i] *= 2024
-                    }
-                },
-            }
-        }
-    }
-
-    Some(stones.len() as u32)
-}
-
-/*
-The number of total stone can be counted while being generated. Every time a split happen,
-one is added to the previous count of the previous iteration.
-The previous count of the first iteration is the number of starting stones.
-*/
-
-// from https://users.rust-lang.org/t/how-to-wrap-a-non-object-safe-trait-in-an-object-safe-one/33904/5
-#[derive(Copy, Clone)]
-struct BlinkIterator {
-    stone: Option<u64>,
-    blinks: usize,
-}
-
-impl BlinkIterator {
-    pub fn new(i: Option<u64>, depth: usize) -> Self {
-        Self {
-            stone: i,
-            blinks: depth,
-        }
-    }
-}
-impl ParallelIterator for BlinkIterator {
-    type Item = u64;
-    fn drive_unindexed<C>(self, consumer: C) -> C::Result
-    where
-        C: UnindexedConsumer<Self::Item>,
-    {
-        if self.stone == None {
-            let folder = consumer.into_folder();
-            folder.complete()
-        } else if self.blinks == 0 {
-            let folder = consumer.into_folder();
-            folder.consume(self.stone.unwrap()).complete()
-        } else {
-            let (left, right, reducer) = consumer.split_at(1 << (self.blinks - 1));
-
-            let depth = self.blinks - 1;
-            let (iter1, iter2) = match self.stone {
-                Some(0) => (
-                    BlinkIterator::new(Some(1), depth),
-                    BlinkIterator::new(None, depth),
-                ),
-                Some(number) => {
-                    let mut digits = 0;
-                    let mut rest = number;
-
-                    while rest > 0 {
-                        rest /= 10;
-                        digits += 1;
-                    }
-
-                    if digits & 1 == 0 {
-                        let half_power = 10u64.pow(digits / 2);
-                        (
-                            BlinkIterator::new(Some(number / half_power), depth),
-                            BlinkIterator::new(Some(number % half_power), depth),
-                        )
-                    } else {
-                        (
-                            BlinkIterator::new(Some(number * 2024), depth),
-                            BlinkIterator::new(None, depth),
-                        )
-                    }
-                },
-                _ => unreachable!(),
-            };
-
-            let (res1, res2) = rayon::join(
-                move || iter1.drive_unindexed(left),
-                move || iter2.drive_unindexed(right),
-            );
-            reducer.reduce(res1, res2)
-        }
+fn cached(args: (u64, u32), cache: &mut HashMap<(u64, u32), u64>, max_depth: u32) -> u64 {
+    if let Some(v) = cache.get(&args) {
+        *v
+    } else {
+        let v = blink(args, cache, max_depth);
+        cache.insert(args, v);
+        v
     }
 }
 
-pub fn part_two(input: &str) -> Option<u32> {
-    let (_, mut stones) = parse(input).ok()?;
+fn blink(args: (u64, u32), cache: &mut HashMap<(u64, u32), u64>, max_depth: u32) -> u64 {
+    if args.1 >= max_depth {
+        return 0;
+    };
 
-    Some(
-        stones
-            .into_par_iter()
-            .flat_map(|x| BlinkIterator::new(Some(x), 55))
-            .count() as u32,
-    )
+    if args.0 == 0 {
+        return cached((1, args.1 + 1), cache, max_depth);
+    }
+
+    let mut digits = 0;
+    let mut rest = args.0;
+
+    while rest > 0 {
+        rest /= 10;
+        digits += 1;
+    }
+
+    if digits & 1 == 0 {
+        let half_power = 10u64.pow(digits / 2);
+        let left = args.0 / half_power;
+        let right = args.0 % half_power;
+
+        let a = cached((left, args.1 + 1), cache, max_depth);
+        let b = cached((right, args.1 + 1), cache, max_depth);
+
+        return a + b + 1;
+    }
+
+    cached((args.0 * 2024, args.1 + 1), cache, max_depth)
+}
+
+pub fn part_one(input: &str) -> Option<u64> {
+    let (_, stones) = parse(input).ok()?;
+    let mut sum = stones.len() as u64;
+    let mut cache = HashMap::new();
+
+    for stone in stones {
+        sum += blink((stone, 0), &mut cache, 25)
+    }
+
+    Some(sum)
+}
+
+pub fn part_two(input: &str) -> Option<u64> {
+    let (_, stones) = parse(input).ok()?;
+    let mut sum = stones.len() as u64;
+    let mut cache = HashMap::new();
+
+    for stone in stones {
+        sum += blink((stone, 0), &mut cache, 75)
+    }
+
+    Some(sum)
 }
 
 #[cfg(test)]
@@ -145,6 +88,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(65601038650482));
     }
 }
