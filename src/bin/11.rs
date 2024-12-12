5c286936234c101e9940204cf35256aa3337970b
zUnixorn <dev@jonascrull.de>
2024-12-12 02:05:11 +0100

    clean code up with `cached` crate

--- src/bin/11.rs
+++ src/bin/11.rs
@@ -2,7 +2,7 @@ use advent_of_code::number;
 use nom::bytes::complete::tag;
 use nom::multi::separated_list0;
 use nom::IResult;
-use std::collections::HashMap;
+use cached::proc_macro::cached;
 
 advent_of_code::solution!(11);
 
@@ -10,27 +10,18 @@ fn parse(input: &str) -> IResult<&str, Vec<u64>> {
     separated_list0(tag(" "), number)(input.trim())
 }
 
-fn cached(args: (u64, u32), cache: &mut HashMap<(u64, u32), u64>, max_depth: u32) -> u64 {
-    if let Some(v) = cache.get(&args) {
-        *v
-    } else {
-        let v = blink(args, cache, max_depth);
-        cache.insert(args, v);
-        v
-    }
-}
-
-fn blink(args: (u64, u32), cache: &mut HashMap<(u64, u32), u64>, max_depth: u32) -> u64 {
-    if args.1 >= max_depth {
+#[cached]
+fn blink_stone(stone: u64, depth: u32, max_depth: u32) -> u64 {
+    if depth >= max_depth {
         return 0;
     };
 
-    if args.0 == 0 {
-        return cached((1, args.1 + 1), cache, max_depth);
+    if stone == 0 {
+        return blink_stone(1, depth + 1, max_depth);
     }
 
     let mut digits = 0;
-    let mut rest = args.0;
+    let mut rest = stone;
 
     while rest > 0 {
         rest /= 10;
@@ -39,40 +30,38 @@ fn blink(args: (u64, u32), cache: &mut HashMap<(u64, u32), u64>, max_depth: u32)
 
     if digits & 1 == 0 {
         let half_power = 10u64.pow(digits / 2);
-        let left = args.0 / half_power;
-        let right = args.0 % half_power;
+        let left = stone / half_power;
+        let right = stone % half_power;
 
-        let a = cached((left, args.1 + 1), cache, max_depth);
-        let b = cached((right, args.1 + 1), cache, max_depth);
+        let a = blink_stone(left, depth + 1, max_depth);
+        let b = blink_stone(right, depth + 1, max_depth);
 
         return a + b + 1;
     }
 
-    cached((args.0 * 2024, args.1 + 1), cache, max_depth)
+    blink_stone(stone * 2024, depth + 1, max_depth)
+}
+
+fn blink(stones: Vec<u64>, times: u32) -> u64 {
+    let mut sum = stones.len() as u64;
+
+    for stone in stones {
+        sum += blink_stone(stone, 0, times)
+    }
+
+    sum
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
     let (_, stones) = parse(input).ok()?;
-    let mut sum = stones.len() as u64;
-    let mut cache = HashMap::new();
 
-    for stone in stones {
-        sum += blink((stone, 0), &mut cache, 25)
-    }
-
-    Some(sum)
+    Some(blink(stones, 25))
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
     let (_, stones) = parse(input).ok()?;
-    let mut sum = stones.len() as u64;
-    let mut cache = HashMap::new();
 
-    for stone in stones {
-        sum += blink((stone, 0), &mut cache, 75)
-    }
-
-    Some(sum)
+    Some(blink(stones, 75))
 }
 
 #[cfg(test)]
